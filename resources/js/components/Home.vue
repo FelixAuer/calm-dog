221fc069f49ab6655d2382ac7d59ff2f79b13cf0
Felix Auer <felixkarlauer@gmail.com>
2020-07-18 13:07:56 +0200

    Add warning for bigger screens

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <div class="hidden md:block text-center mb-4">
+            <div class="px-4 py-2 bg-red-400 items-center text-red-100 leading-none lg:rounded-full flex lg:inline-flex"
+                 role="alert">
+                <span
+                    class="text-left flex-auto">KalmKanine is designed for smartphones in portait mode and might not be displayed correctly on larger screens.</span>
+            </div>
+        </div>
         <div class="mb-4">
             <div class="bg-white rounded-lg p-4 shadow-md" v-if="maxId">
                 <router-link :to="{name: 'exercise', params: {id: maxId}}">
@@ -44,11 +51,12 @@
                     <router-link :to="{name: 'exercise', params: {id: exercise.id}}">
                         {{ exercise.name }}
                         <div class="inline" v-if="exercise.id < maxId">
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check rounded-full bg-yellow-500 inline" fill="currentColor"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                  d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
-                        </svg>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16"
+                                 class="bi bi-check rounded-full bg-yellow-500 inline" fill="currentColor"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                      d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+                            </svg>
                         </div>
                     </router-link>
                 </li>
